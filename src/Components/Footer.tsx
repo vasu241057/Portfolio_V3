4506e10b21dcd5ff79de0a253819f5e0a5e5842c
Vasu Khandelwal <f20190700@pilani.bits-pilani.ac.in>
2024-09-14 21:31:22 +0530

    update

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Footer: React.FC = () => {
   return (
     <footer className="mt-6 text-center text-sm text-gray-500 pb-4">
--- src/Components/Tabscontent.tsx
+++ src/Components/Tabscontent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const TabsComponent = () => {
   const [activeTab, setActiveTab] = useState("Profile");
